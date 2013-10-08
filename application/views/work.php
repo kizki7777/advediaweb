ea61b8ff41d562a0b9cc8cdd3e0464753ce86e86
Kris Mulyandani Putra <kris.mp7777@gmail.com>
2013-10-08 12:32:56 +0700

    assets update for work page

--- application/views/work.php
+++ application/views/work.php
@@ -6,7 +6,7 @@
 	</header>
 </div>
 
-<section  class="page bg-yellow links--red page--overflow adv-custom-distinct">
+<section  class="page links--red page--overflow adv-custom-distinct">
 	<div class="page__body constrict greeting">
 		<div class="welcome-right">
 			<h1>Distinction That Matters</h1>
@@ -20,14 +20,15 @@
 
 <h2 class="dividing-header text-yellow">our works</h2>
 
-<section  class="page links--red page--overflow adv-custom-yellow">	
-	<div class="page__body constrict">
+<section  class="page page--overflow adv-custom-yellow">	
+	<div class="page__body constrict adv-prolog">
 		<article class="copy-block copy-block--right">
-			<h2 class="block-heading">South Tees Hospitals</h2>
-			<p class="intro">NHS Foundation Trust</p>
-			<p>We broke new ground in producing this excellent website; taking an NHS Trust for the very first time into the world of responsive web design to embrace the increase in mobile and tablet use among the site’s users. Extensive user research allowed us to create a model of usability from highly complex content requirements.</p>
-			<a href="southtees" class="btn">Read case study</a><br/>			
-			<a href="http://www.southtees.nhs.uk" class="btn">visit website</a>			
+			<img src="<?php echo base_url() ?>assets/img/logo-uad.png" class="logo-box">
+			<h2 class="block-heading">Website U-Ad</h2>
+			<p class="intro">ui/ux design | development | back end development</p>
+			<p>Melihat peningkatan jumlah pengguna internet dan perkembangan industri media, Telkom Indonesia melakukan pengembangan bisnis media berbasis internet dengan produk U-Ad karena mereka percaya everything is advertising. Telkom Indonesia mempercayakan kepada Advedia untuk membangun sebuah website yang berfungsi sebagai rumah informasi bagi para publisher dan pengiklan.</p>
+			<p>Bagi para pengiklan, web ini mau mengatakan bahwa U-Ad menyediakan varian digital placement yang bisa digunakan oleh para pemasar untuk meningkatkan eksposure dari iklan mereka. Sedangkan bagi para publisher, website ini mau mengatakan bahwa U-Ad menawarkan kerja sama dalam hal penempatan iklan di blog ataupun website dengan cara yang sama-sama menguntungkan.</p>
+			<a href="http://www.u-ad.info" class="adv-visit-btn"></a>
 		</article>
 		<div class="product product--left product--macbook">
 			<img src="<?php echo base_url() ?>assets/img/uad.png" alt="South Tees" class="product__mount">
@@ -35,14 +36,13 @@
 	</div>
 </section>
 
-<section  class="page links--red page--overflow adv-custom-white">	
-	<div class="page__body constrict">
+<section  class="page page--overflow adv-custom-white">	
+	<div class="page__body constrict adv-prolog">
 		<article class="copy-block copy-block--left">
-			<h2 class="block-heading">South Tees Hospitals</h2>
-			<p class="intro">NHS Foundation Trust</p>
-			<p>We broke new ground in producing this excellent website; taking an NHS Trust for the very first time into the world of responsive web design to embrace the increase in mobile and tablet use among the site’s users. Extensive user research allowed us to create a model of usability from highly complex content requirements.</p>
-			<a href="southtees" class="btn">Read case study</a><br/>			
-			<a href="http://www.southtees.nhs.uk" class="btn">visit website</a>			
+			<img src="<?php echo base_url() ?>assets/img/logo-sales.png" class="logo-landscape">
+			<h2 class="block-heading">Aplikasi Sales</h2>
+			<p class="intro">development</p>
+			<p>Untuk mendukung sistem penjualan U-Ad, PT Telkom Indonesia mempercayakan kepada kami pembuatan aplikasi sales yang berfungsi untuk mempermudah perusahaan dalam mengelola data pesanan media placement U-Ad.</p>
 		</article>
 		<div class="product product--right product--macbook">
 			<img src="<?php echo base_url() ?>assets/img/sales.png" alt="South Tees" class="product__mount">
@@ -50,14 +50,13 @@
 	</div>
 </section>
 
-<section  class="page links--red page--overflow adv-custom-md">	
-	<div class="page__body constrict">
+<section  class="page page--overflow adv-custom-md">	
+	<div class="page__body constrict adv-prolog">
 		<article class="copy-block copy-block--right">
-			<h2 class="block-heading">South Tees Hospitals</h2>
-			<p class="intro">NHS Foundation Trust</p>
-			<p>We broke new ground in producing this excellent website; taking an NHS Trust for the very first time into the world of responsive web design to embrace the increase in mobile and tablet use among the site’s users. Extensive user research allowed us to create a model of usability from highly complex content requirements.</p>
-			<a href="southtees" class="btn">Read case study</a><br/>			
-			<a href="http://www.southtees.nhs.uk" class="btn">visit website</a>			
+			<img src="<?php echo base_url() ?>assets/img/logo-md.png" class="logo-box">
+			<h2 class="block-heading">Muhammadiyah App</h2>
+			<p class="intro">ui/ux design | development | backend development</p>
+			<p>Sebagai sebuah organisasi yang besar, Muhammadiyah ingin lebih mendekatkan diri dengan para anggotanya. Atas dasar itu, muhamadiah mempercayakan kepada kami pembuatan aplikasi muhamadiah. Di dalam aplikasi ini terdapat fitur bernuansa agama yang dapat diakses dengan internet melalui seluler mereka. Selain itu aplikasi ini juga menyediakan layanan berbayar untuk tausiah dan tanya jawab.</p>			
 		</article>
 		<div class="product product--left product--macbook">
 			<img src="<?php echo base_url() ?>assets/img/md.png" alt="South Tees" class="product__mount">
@@ -65,17 +64,62 @@
 	</div>
 </section>
 
-<section  class="page bg--white links--red page--overflow"  >	
-	<div class="page__body constrict">
+<section  class="page bg--white links--red page--overflow adv-custom-uni">	
+	<div class="page__body constrict adv-prolog">
 		<article class="copy-block copy-block--left">
-			<h2 class="block-heading">South Tees Hospitals</h2>
-			<p class="intro">NHS Foundation Trust</p>
-			<p>We broke new ground in producing this excellent website; taking an NHS Trust for the very first time into the world of responsive web design to embrace the increase in mobile and tablet use among the site’s users. Extensive user research allowed us to create a model of usability from highly complex content requirements.</p>
-			<a href="southtees" class="btn">Read case study</a><br/>			
-			<a href="http://www.southtees.nhs.uk" class="btn">visit website</a>			
+			<img src="<?php echo base_url() ?>assets/img/logo-unilever.png" class="logo-box">
+			<h2 class="block-heading">Unilever</h2>
+			<h4 class="block-heading">Free wifi login page</h4>
+			<p class="intro">ui/ux design | development | responsive</p>
+			<p>Menyadari tingginya penggunaan internet termasuk melalui fasilitas Wi-Fi, Unilever menjalin kerjasama dengan Advedia Solusi Informatika dalam rangka memberikan kemudahan akses internet gratis melalui aplikasi unilever wi-fi. Aplikasi ini berfungsi sebagai pintu masuk sebelum pengguna dapat mengakses internet gratis.</p>		
 		</article>
 		<div class="product product--right product--macbook">
 			<img src="<?php echo base_url() ?>assets/img/unilever.png" alt="South Tees" class="product__mount">
+		</div>
+	</div>
+</section>
+
+<section  class="page bg--white links--red page--overflow">	
+	<div class="page__body constrict adv-prolog">
+		<article class="copy-block copy-block--right">
+			<img src="<?php echo base_url() ?>assets/img/logo-canda.png" class="logo-landscape">
+			<h2 class="block-heading">CSR</h2>
+			<h4 class="block-heading">Ceria Anak Indonesia (CANDA)</h4>
+			<p class="intro">ui/ux design | development | graphic design | event organizer</p>
+			<p>Sebagai sebuah perusahaan yang bergerak dan bersentuhan dengan jaringan internet, kami menyadari bahwa internet tidak selalu membawa dampak yang positif. Semakin dekatnya internet dengan masyarakat, semakin besar pula kemungkinan terkena dampak negatifnya, termasuk bagi anak-anak. Atas dasar itulah kami saat ini merancang dan merencanakan pembuatab website ceria anak indonesia. Dengan adanya website ini diharapkan anak-anak memiliki alternatif tujuan berinternet yang sehat.</p>		
+		</article>
+		<div class="product product--left product--macbook">
+			<img src="<?php echo base_url() ?>assets/img/canda.png" alt="South Tees" class="product__mount">
+		</div>
+	</div>
+</section>
+
+<section  class="page page--overflow adv-custom-kpdt">	
+	<div class="page__body constrict adv-prolog">
+		<article class="copy-block copy-block--left">
+			<img src="<?php echo base_url() ?>assets/img/logo-kpdt.png" class="logo-landscape">
+			<h2 class="block-heading">Website</h2>
+			<h4 class="block-heading">Kementerian Pembangunan Daerah Tertinggal</h4>
+			<p class="intro">ui/ux design | development</p>
+			<p>Dalam rangka mendukung program kerja kementrian PDT, kami dipercaya untuk membangun website KPDT. Website ini berfungsi sebagai sebuah informasi tentang seluruh kegiatan yang dilakukan oleh kementrian PDT.</p>
+			<a href="http://www.u-ad.info" class="adv-visit-btn"></a>			
+		</article>
+		<div class="product product--right product--macbook">
+			<img src="<?php echo base_url() ?>assets/img/kpdt-ss.png" alt="South Tees" class="product__mount">
+		</div>
+	</div>
+</section>
+
+<section  class="page page--overflow">	
+	<div class="page__body constrict adv-prolog">
+		<article class="copy-block copy-block--left">
+			<h2 class="block-heading">Digital</h2>
+			<h4 class="block-heading">Media Placement</h4>
+			<p>Advedia Solusi Informatika melengkapi layanan dalam hal solusi informasi dengan penawaran berbagai pilihan digital media placement. Kami telah menjaring relasi dengan berbagai publisher yang dapat Anda pilih sebagai alternatif media placement sesuai kebutuhan</p>			
+		</article>
+		<div class="product product--right product--macbook">
+			<img src="<?php echo base_url() ?>assets/img/macbook.png" alt="South Tees" class="product__mount">
+			<img src="<?php echo base_url() ?>assets/img/fortuner.jpg" alt="South Tees" class="product__screenshot">
 		</div>
 	</div>
 </section>
