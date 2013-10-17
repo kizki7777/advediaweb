9fb7685602177401da2f2f0100c82acd9d066ceb
Kris Mulyandani Putra <kris.mp7777@gmail.com>
2013-10-17 10:10:41 +0700

    edit map

--- application/views/contact.php
+++ application/views/contact.php
@@ -90,7 +90,7 @@
 
 <script>
 
-	var map = L.map('map').setView([-6.27559, -0.09], 13);
+	var map = L.map('map').setView([51.505, -0.15], 13);
 
 	L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
 		maxZoom: 18,
@@ -124,5 +124,4 @@
 	}
 
 	map.on('click', onMapClick);
-
 </script>
